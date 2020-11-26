2c85920e7c2ee2b5af8f8e195714ba1dd0417b6a
cristian <cristianmedinaranda@hotmail.com>
2020-11-26 01:42:07 -0300

    primer comit

---
+++ enviar.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>DiseñoAdaptados| Inicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Estilos Css -->
+    <link rel="stylesheet" href="css/styles.css">
+  
+
+    <link href="https://fonts.googleapis.com/css?family=Red+Hat+Text:400,500,700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+  <!-- Ir Arriba -->
+  <div class="go-top">
+    <span class="fas fa-angle-up"></span>
+  </div>
+
+    <!-- Menu de Navegacion -->
+    <header id="header">
+    <nav class="menu">
+     <div class="logo-box">
+       <h2>DiseñoAdaptados</h2>
+       <!--<img src="img/logo.jpg" width="140px" height="60px" alt="logo">-->
+       <span class="btn-menu"><i class="fas fa-bars"></i></span>
+     </div>
+     
+     <div class="list-container">
+        <ul class="lists">
+            <li><a href="#inicio"class="active">Inicio</a></li>
+            <li><a href="#nosotros">Nosotros</a></li>
+            <li><a href="#servicio">Servicios</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contactos">Contacto</a></li>
+        </ul>
+     </div>
+    </nav>
+ 
+    <!-- Imagen Header -->
+    <div class="img-header">
+     <div class="welcome">
+       <h1>El mensaje se envio correctamente </h1>
+       <hr>
+       <p>Diseño y Desarrollo web</p>
+       <button id="abajo">Ver abajo</button>
+     </div>       
+    </div>
+    
+    <div class="skew-abajo"></div>
+
+    </header>
+
+ <main>
+
+    <!-- Acerca de nosotros -->
+    <section class="acerca-de">
+    <div class="info-container">
+      <h1 id="nosotros">Acerca de Nosotros</h1>
+      <p> Somos un grupo de trabajo joven, conformado por tecnicos , diseñadores, programadores y community managers que realizamos el diseño de paginas web adaptadas a cualquier dispositivo.
+        Nuestro desafío es crecer día a día y estar actualizado a las últimas tendencias en usabilidad, diseño y tecnología.
+        Para entender el enfoque y visión de una empresa hay que sumergirse en su mundo y entenderla. Por ello brindamos soluciones efectivas acordes a cada organización priorizando la imágen y la visibilidad de la misma.
+        Acompañamos junto a un gran diseño una estrategia de marketing digital que abarca desde redes sociales, anuncios y posicionamiento orgánico. Cada elemento es fundamental para crear una red y tener mayor visibilidad a nivel global.
+     </p>
+      <div class="about-gallery">
+        <img src="img/img-9.jpg" alt="">
+        <img src="img/img-13.jpg" alt="">
+        <img src="img/img-17.jpg" alt="">
+      </div>
+    
+
+    </div>
+    </section>
+
+   <!-- Nuestros proyectos -->
+   <section class="our-projects">
+      <div class="skew-arriba"></div>
+   <div class="deg-background"></div>
+   
+   <div class="ejeZproject">
+      <div class="container-project">
+        <div class="project-title">
+          <h2 id="servicio">Nuestros  Servicios</h2>
+          <hr>
+        </div>
+          <div class="project-img">
+            
+            <img src="img/theme-2.png" alt="">
+            <img src="img/theme-3.png" alt="">
+            <img src="img/theme-4.jpg" alt="">
+            <img src="img/theme-5.png" alt="">
+            <img src="img/theme-6.jpg" alt="">
+            <img src="img/theme-7.jpg" alt="">
+            <img src="img/theme-8.png" alt="">
+            <img src="img/theme-9.jpg" alt="">
+            <img src="img/theme-10.jpg" alt="">
+         </div>
+   <div class="skew-abajo"></div>
+   </section>
+
+   <div style="text-align: center; font-size: 45px;">
+     <h1 class="precio">Pagina web $2000</h1>
+    <!-- <span class="descripcion">4 enlaces + formulario de contacto + redes sociales + medios de pagos</span>-->
+    <p class="precio">4 enlaces + formulario de contacto + redes sociales + medios de pagos +dominio + diseño resposive </p>
+   </div>
+   <br>
+   <br>
+   <br>
+   <br>
+   <div class="comprar">
+
+  <a mp-mode="dftl" href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=355770895-d147e87a-abe6-4c08-852f-16939a34a078" name="MP-payButton" class='blue-ar-l-sq-arall' target="blank">Comprar</a>
+  <script type="text/javascript">
+  (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+  </script>
+</div>
+
+   <!-- Testimonios -->
+   <section class="testimonios">
+    <div class="testimonios-title">
+     <h2 id="blog">Testimonios</h2>
+     <hr>
+    </div>
+
+     <div class="box-testimonio">
+      <div class="card-testimonio">
+        <div class="card-img"><img src="img/peaple-1.jpg" alt=""></div>
+        <div class="testimonio-text">
+        <h4>Amanta Castillo</h4>
+          
+        <p> "fácil, y con muchas opciones!! Estoy muy contenta del resultado final y del trato recibido por las personas que forman DesigneAdaptados! Gracias!"</p>
+        </div>
+      </div>
+
+      <div class="card-testimonio">
+          <div class="card-img"><img src="img/peaple-2.jpg" alt=""></div>
+          <div class="testimonio-text">
+          <h4>Clara Smilla</h4>
+          <p>"Muy bien desde el principio y ha sabido captar a la perfección lo que yo quería reflejar en mi logo. Gran profesional, atento a las tendencias y de trato personal cercano. Delicado y elegante en sus diseños. Totalmente recomendable, volvería sin dudarlo".</p>
+          </div>
+        </div>
+
+        <div class="card-testimonio">
+            <div class="card-img"><img src="img/peaple-3.jpg" alt=""></div>
+            <div class="testimonio-text">
+            <h4>Oscar Muñoz</h4>
+            <p>Estamos encantados con el trabajo en nuestra página web. Ha sabido encontrar una solución para cada uno de nuestros requerimientos y conseguir el producto que deseábamos.
+              Agradecemos mucho su amabilidad y atención para dar buenos consejos y resolvernos cualquier duda. Un profesional totalmente recomendable".</p>
+            </div>
+          </div>
+
+          <div class="card-testimonio">
+              <div class="card-img"><img src="img/peaple-4.jpg" alt=""></div>
+              <div class="testimonio-text">
+              <h4>Roberto Perez</h4>
+              <p>"Más que satisfecho con el servicio y el trato. Atento, amable y agradable. Genera confianza desde el primer momento. Los resultados se ven cuidados y se nota que dedica tiempo al trabajo solicitado. Para mí, sin duda, muy recomendable. Y para muestra la web que me ha hecho. Lo dicho, un cinco estrellas."</p>
+              </div>
+            </div>
+
+     </div>
+   </section>
+
+ </main> 
+
+ <!-- Footer -->
+ <footer class="footer">
+   <div class="skew-arriba"></div>
+  <div class="deg-footer"></div>
+ 
+  <div class="ejeZfooter">
+  <div class="footer-content">
+   <div class="footer-title">
+     <h2 id="contactos">Contacto</h2>
+     <hr>
+   </div>
+
+<?php 
+$myemail = 'secopley7@gmail.com';
+$name = $_POST['user'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = $myemail;
+$email_subject = "Nuevo mensaje: $subject";
+$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+$headers = "From: $email";
+
+mail($to, $email_subject, $email_body, $headers);
+echo "El mensaje se ha enviado correctamente";
+?>
+
+
+   <div class="formulario-content">
+     
+     <form method="POST" action="enviar.php" id="formulario">
+        <label for="user">Nombre:</label>
+        <input type="text" id="user" name="user" placeholder="Ingresa tu Nombre" required >
+   
+        <label for="email">Correo Electronico:</label>
+        <input type="email" id="email" name="email" placeholder="Ingresa tu Correo Electronico" required>
+
+        <label for="message">Escribe tu mensaje</label>
+        <textarea name="menssage" id="message" required></textarea>
+   
+        <div class="send"><button type="submit">Enviar</button></div>
+   
+        <div class="mensaje-form">
+          
+        </div>
+     </form>
+   </div>
+ 
+   <div class="social">
+  
+     <ul>
+       <li><a href="https://www.facebook.com/profile.php?id=100041447220576" target="blank" class="fab fa-facebook-f"></a></li>
+       <li><a href="https://twitter.com/designeadaptad1" target="blank" class="fab fa-twitter"></a></li>
+       <li><a href="https://www.instagram.com/designeadaptados/" target="blank" class="fab fa-instagram"></a></li>
+       <li><a href="https://m.me/100041447220576" target="blank" class="fab fa-facebook-messenger"></a></li>
+       <li><a href="https://wa.me/5491136938447" target="blank" class="fab fa-whatsapp"></a></li>
+       
+       </div>
+       </ul>
+     </div>
+   
+   <div class="footer-text">
+     <p>&copy; DesigneAdaptados - 2020 | Todos los derechos reservados.</p>
+   </div>
+ </footer>
+
+<script src="https://kit.fontawesome.com/35db202371.js"></script>
+<script src="js/app.js"></script>
+ </footer>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.224830473843!2d-58.63495268539173!3d-34.67427468044132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc7398c9e1be9%3A0xfab482c3d80eb355!2sAvelino%20Palacios%202376%2C%20B1712MDT%20Castelar%2C%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1587533292656!5m2!1ses!2sar"
+ width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+
+</iframe>
+
+</body>
+
+</html>
